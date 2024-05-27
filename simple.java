ddf0b5dd099154fc55d2fd7e6d62cb32173903a5
subhayan patra <155102668+PATRACODER@users.noreply.github.com>
2024-05-27 08:18:04 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class simple {
+ 
+    public static void main(String[] args) {
+        // System.out.println("hello");
+
+
+        // Scanner input = new Scanner(System.in);
+        // System.out.println(input.nextInt());
+
+
+        // int rollnumber = 7;
+        // float marks = 23.45f;
+        // double precesion = 45.653124;
+        // long value = 52436346273436753L;
+        // boolean check = true;
+        // char name = 'subhayan';
+
+
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Please enter the roll number :");
+        // int rollnumber = input.nextInt();
+        // System.out.println("My roll number is : " + rollnumber);
+
+
+        //  SUM OF TWO NUMBER : 
+
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int sum1 = input.nextInt();
+        System.out.print("Enter the second number : ");
+        int sum2 = input.nextInt();
+        int sum = sum1 + sum2;
+        System.out.println("The sum of two number is :" + sum);
+
+
+    }
+}
+
